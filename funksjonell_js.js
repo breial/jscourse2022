df178ebfa3b32229ed0f22be47b94eb21098162d
Therese Larsen <therese@bacon.no>
2022-09-21 11:25:29 +0200

    La til test av reduce, map og transaksjon

--- funksjonell_js.js
+++ funksjonell_js.js
@@ -1,40 +1,72 @@
-// Et enkelt array med json-objekter. Nå er vi i objektorientering og funksjonell utvikling.
-let dyr = [
-    {"navn":"Petter","art":"potet","alder":4},
-    {"navn":"Peik","art":"potet","alder":5},
-    {"navn": "Kari","art":"enhjørning","alder":7},
-    {"navn":"Truls","art":"larve","alder":8}
-];
+// // Et enkelt array med json-objekter. Nå er vi i objektorientering og funksjonell utvikling.
+// let dyr = [
+//     {"navn":"Petter","art":"potet","alder":4},
+//     {"navn":"Peik","art":"potet","alder":5},
+//     {"navn": "Kari","art":"enhjørning","alder":7},
+//     {"navn":"Truls","art":"larve","alder":8}
+// ];
 
-// Hent deler av et JSON-objekt med map
+// // Hent deler av et JSON-objekt med map
 
-dyr.map( d => d.navn+" er en "+d.art+" som er "+d.alder+" år gammel")
+// dyr.map( d => d.navn+" er en "+d.art+" som er "+d.alder+" år gammel")
 
-console.log(dyr.map( d => d.navn+" er en "+d.art+" som er "+d.alder+" år gammel"));
+// console.log(dyr.map( d => d.navn+" er en "+d.art+" som er "+d.alder+" år gammel"));
 
-console.log("-".repeat(40));
+// console.log("-".repeat(40));
 
-dyr.forEach(x => console.log(x.navn,"er en",x.art));
+// dyr.forEach(x => console.log(x.navn,"er en",x.art));
 
-// Hvis jeg bare vil se dyrenavn - et nytt array som bare viser navn. Mapper et element
-// Et array med arrays er å ha to x inni her
-let dyrenavn = dyr.map(x => [x.navn,x.alder]);
+// // Hvis jeg bare vil se dyrenavn - et nytt array som bare viser navn. Mapper et element
+// // Et array med arrays er å ha to x inni her
+// let dyrenavn = dyr.map(x => [x.navn,x.alder]);
 
-console.log(dyrenavn);
+// console.log(dyrenavn);
 
-// Hva hvis jeg vil ha dette ut som tekst og ikke array? Da bruker vi join
-console.log(dyrenavn.join("\n"));
+// // Hva hvis jeg vil ha dette ut som tekst og ikke array? Da bruker vi join
+// console.log(dyrenavn.join("\n"));
 
-console.log("-".repeat(40));
+// console.log("-".repeat(40));
 
-// Filter - denne vil kun vise den som er potet
-dyr.filter(a => a.art=="potet").forEach(a => console.log(a.navn));
+// // Filter - denne vil kun vise den som er potet
+// dyr.filter(a => a.art=="potet").forEach(a => console.log(a.navn));
 
-console.log("-".repeat(40));
+// console.log("-".repeat(40));
 
-// To likhetstegn sjekker
-// Tre likhetstegn setter den til å være det.
+// // To likhetstegn sjekker
+// // Tre likhetstegn setter den til å være det.
 
-const kattenavn = dyr.filter(a => a.art=="potet").map(x =>x.navn).sort().reverse();
+// const kattenavn = dyr.filter(a => a.art=="potet").map(x =>x.navn).sort().reverse();
 
-console.log(kattenavn);
+// console.log(kattenavn);
+
+// console.log("-".repeat(40));
+
+// // La oss se på reduce
+
+// let snittalder = dyr.map(a => a.alder).reduce((a,b) => (a + b)) / dyr.length;
+
+// console.log("Snittalder: "+snittalder);
+
+// console.log("-".repeat(40));
+
+// Nå skal vi kose oss med bankapplikasjon
+
+let konto = [
+
+    { "nummer": 1001, "transaksjon": 600 },
+    { "nummer": 1002, "transaksjon": 300 },
+    { "nummer": 1002, "transaksjon": 700 },
+    { "nummer": 1001, "transaksjon": -200 },
+    { "nummer": 1001, "transaksjon": -600 },
+    { "nummer": 1001, "transaksjon": 6000 },
+]
+
+let saldo_1001 = konto.filter( a => a.nummer == 1001).map( a => a.transaksjon).reduce((a,b) => a+b);
+
+console.log(saldo_1001);
+
+//Hint: ikke begynn en variabel med tall
+let t1002_01 = konto.find( a => a.nummer == 1002).transaksjon;
+
+console.log(t1002_01);
+// Denne viser den første transaksjonen til konto 1002
