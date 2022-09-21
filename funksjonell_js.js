f97fa3836f46e177663d4cea5485817ce48e1374
Therese Larsen <therese@bacon.no>
2022-09-21 11:08:24 +0200

    Test av filter

--- funksjonell_js.js
+++ funksjonell_js.js
@@ -1,11 +1,13 @@
 // Et enkelt array med json-objekter. Nå er vi i objektorientering og funksjonell utvikling.
 let dyr = [
     {"navn":"Petter","art":"potet","alder":4},
+    {"navn":"Peik","art":"potet","alder":5},
     {"navn": "Kari","art":"enhjørning","alder":7},
     {"navn":"Truls","art":"larve","alder":8}
 ];
 
-//Vi skal nå bruke map
+// Hent deler av et JSON-objekt med map
+
 dyr.map( d => d.navn+" er en "+d.art+" som er "+d.alder+" år gammel")
 
 console.log(dyr.map( d => d.navn+" er en "+d.art+" som er "+d.alder+" år gammel"));
@@ -13,3 +15,26 @@ console.log(dyr.map( d => d.navn+" er en "+d.art+" som er "+d.alder+" år gammel
 console.log("-".repeat(40));
 
 dyr.forEach(x => console.log(x.navn,"er en",x.art));
+
+// Hvis jeg bare vil se dyrenavn - et nytt array som bare viser navn. Mapper et element
+// Et array med arrays er å ha to x inni her
+let dyrenavn = dyr.map(x => [x.navn,x.alder]);
+
+console.log(dyrenavn);
+
+// Hva hvis jeg vil ha dette ut som tekst og ikke array? Da bruker vi join
+console.log(dyrenavn.join("\n"));
+
+console.log("-".repeat(40));
+
+// Filter - denne vil kun vise den som er potet
+dyr.filter(a => a.art=="potet").forEach(a => console.log(a.navn));
+
+console.log("-".repeat(40));
+
+// To likhetstegn sjekker
+// Tre likhetstegn setter den til å være det.
+
+const kattenavn = dyr.filter(a => a.art=="potet").map(x =>x.navn).sort().reverse();
+
+console.log(kattenavn);
